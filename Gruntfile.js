7b8cd1931afd3170c378c869a28f694347f80005
Kirill Rublevsky <rubkirill@gmail.com>
2015-07-06 21:49:52 +0300

    second frontend commit

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,18 +1,85 @@
 module.exports = function(grunt) {
+	
+	require('load-grunt-tasks')(grunt);
 
 	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		clean: ['build'],
+		copy:{
+			build: {
+				src: 'src/index.html', 
+				dest: 'build/index.html'
+			}
+		},
+		useminPrepare: {
+			html: 'index.html',
+			options: {
+				dest: 'build'
+			}
+		},
+		ngAnnotate: {
+			options: {
+				singleQuotes: true,
+			},
+			build: {
+				files: {
+					'build/assets/js/script.min.js': ['src/assets/js/*.js'],
+				},
+			},   
+		},
+		concat: {
+			options: {
+				separator: ';'
+			},
+			build: {
+				files: {
+					'build/assets/css/style.min.css': ['src/assets/css/*.css'],
+					'build/assets/js/script.min.js': ['src/assets/js/vendor/*.js', 'build/assets/js/script.min.js'],
+				},
+			},
+		},
+		uglify: {
+			options: {
+				banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+			},
+			build: {
+				src: ['build/assets/js/script.min.js'],
+				dest: 'build/assets/js/script.min.js'
+			}
+		},
+		cssmin: {
+			build: {
+				files: [{
+					src: ['build/assets/css/style.min.css'],
+					dest: 'build/assets/css/style.min.css',
+				}]
+			}
+		},
+		usemin:{
+			build:['build/index.html']
+		},
+		htmlmin: {                                     
+			build: {                                      
+				options: {                                 
+					removeComments: true,
+					collapseWhitespace: true
+				},
+				files: {
+					'build/index.html': 'build/index.html'
+				}
+			}
+		},
 		connect: {
 			server: {
 				options: {
 					port: 9001,
-					base: 'src',
+					base: 'build',
 					keepalive: true
 				}
 			}
 		}
 	});
 
-	grunt.loadNpmTasks('grunt-contrib-connect');
-	grunt.registerTask('default', ['connect']);
+	grunt.registerTask('default', ['clean', 'copy', 'useminPrepare', 'ngAnnotate', 'concat', 'uglify', 'cssmin', 'usemin', 'htmlmin',  'connect']);
 
 };
