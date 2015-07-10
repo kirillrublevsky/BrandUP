11408a8b31d77559e09c8276fd6e3122ad1fce69
Kirill Rublevsky <rubkirill@gmail.com>
2015-07-10 23:07:11 +0300

    eigth frontend commit

--- src/assets/js/firstController.js
+++
@@ -1,6 +0,0 @@
-app.controller("firstController", function($scope) {
-    $scope.message = "";
-    $scope.left  = function() {return 100 - $scope.message.length;};
-    $scope.clear = function() {$scope.message = "";};
-    $scope.save  = function() {alert("Your message was sent. We will response asap");};
-});
