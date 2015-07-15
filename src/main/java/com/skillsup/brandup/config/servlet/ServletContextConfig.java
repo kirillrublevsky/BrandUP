447465537766a0cf4889d7102585aee2ea0011a8
DSkalenko <d.skalenko@gmail.com>
2015-07-15 09:19:32 +0200

    add jetty plugin

--- src/main/java/com/skillsup/brandup/config/servlet/ServletContextConfig.java
+++ src/main/java/com/skillsup/brandup/config/servlet/ServletContextConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 /**
@@ -23,5 +24,10 @@ public class ServletContextConfig extends WebMvcConfigurerAdapter {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
+    }
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("/resources/index.html");
     }
 }
